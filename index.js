16e4749f1a3b5b500e9115bfdedc95ac6275f14b
chaim chazan <chaimyisroel@gmail.com>
2020-08-28 19:05:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+<<<<<<< HEAD
 var katzDeliLine = []
 
 
 function takeANumber (katzDeliLine, currentName) {
   katzDeliLine.push(currentName);
+=======
+var katzDeliLine = ["Ada", ]
+
+
+function takeANumber (katzDeliLine, currentName) {
+>>>>>>> 451328479a775e03a5ad3b41d5c69fec652b8641
   return `Welcome, ${currentName}. You are number ${katzDeliLine.length} in line.`
 }
 
@@ -10,7 +17,11 @@ function nowServing(katzDeliLine) {
   if (katzDeliLine.length > 0) {
     const removedName = katzDeliLine[0]
     katzDeliLine.shift()
+<<<<<<< HEAD
     return `Currently serving ${removedName}.`;
+=======
+    return removedName;
+>>>>>>> 451328479a775e03a5ad3b41d5c69fec652b8641
   } else {
     return "There is nobody waiting to be served!"
   }
@@ -22,7 +33,11 @@ function currentLine(katzDeliLine){
       for (let i = 0; i < katzDeliLine.length; i++) {
         stringLine += ` ${i+1}. ${katzDeliLine[i]},`
       }
+<<<<<<< HEAD
     return stringLine.substring(0, stringLine.length - 1);
+=======
+    return stringLine.substring(0, str.length - 1);
+>>>>>>> 451328479a775e03a5ad3b41d5c69fec652b8641
 
   } else {
     return "The line is currently empty."
